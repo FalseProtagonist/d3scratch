b5f38dd13ebe9093e001df4d0c647bba2d157c94
FalseProtagonist <marclucbeirne@gmail.com>
2015-06-13 23:44:05 +0100

    got d3 enter working

--- src/cljs/chestnut/core.cljs
+++ src/cljs/chestnut/core.cljs
@@ -3,12 +3,15 @@
             [om.dom :as dom :include-macros true]
             [sablono.core :as h :refer-macros [html]]
             [cljs.core.async :refer [<! chan put! sliding-buffer sub pub timeout]]))
-(defonce app-state (atom {:text "Hello D3Scratch7!"}))
+(defonce app-state (atom {:text "Hello D3Scratch!!"}))
 
 
 (def x 4)
 (def TIME_FORMAT
   (-> js/d3 .-time (.format "%Y/%m")))
+
+(defn workingd3 []
+  (-> js/d3 (.select "div") (.selectAll "p") (.data (clj->js ["hello" "world"])) .enter (.append "p") (.text "hello")))
 
 (defn main []
   ()
@@ -17,7 +20,13 @@
       (reify
         om/IRender
         (render [_]
-          (dom/h1 nil (:text app)))))
+          (let [element
+                (dom/div #js {:id "marc"} 
+                         (dom/h1 nil (:text app)))]
+            element))
+        om/IDidMount
+        ( did-mount [_]
+          (workingd3))))
     app-state
     {:target (. js/document (getElementById "app"))}))
 
