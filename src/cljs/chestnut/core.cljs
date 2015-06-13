2d6c1dd8d956d2a56a21857a1311da4354707e2a
FalseProtagonist <marclucbeirne@gmail.com>
2015-06-13 18:41:47 +0100

    Added notes and tried d3

--- src/cljs/chestnut/core.cljs
+++ src/cljs/chestnut/core.cljs
@@ -1,10 +1,17 @@
 (ns chestnut.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [sablono.core :as h :refer-macros [html]]
+            [cljs.core.async :refer [<! chan put! sliding-buffer sub pub timeout]]))
+(defonce app-state (atom {:text "Hello D3Scratch7!"}))
 
-(defonce app-state (atom {:text "Hello Chestnut3!"}))
+
+(def x 4)
+(def TIME_FORMAT
+  (-> js/d3 .-time (.format "%Y/%m")))
 
 (defn main []
+  ()
   (om/root
     (fn [app owner]
       (reify
@@ -13,3 +20,6 @@
           (dom/h1 nil (:text app)))))
     app-state
     {:target (. js/document (getElementById "app"))}))
+
+
+
