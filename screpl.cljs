13615c18640652713f128433b2e6d7767df39165
FalseProtagonist <marclucbeirne@gmail.com>
2015-07-09 02:29:25 +0100

    got d3 graph working!

--- screpl.cljs
+++ screpl.cljs
@@ -1,29 +1,9 @@
-(def width 960)
-(def height 500)
-(def color (-> js/d3 .-scale .category20))
-(def nodes #js [1 2])
-(def )
-(def )
-(def )
-(def )
+(clj->js [{:source 0 :target 1} {:source 0 :target 2}])
 
-(let [width 960 height 500 
-      color (-> js/d3 .-scale .category20)
-      nodes #js [1 2]
-      links #js {:source 0 :target 1}
-      force  (-> js/d3 .-layout .force (.charge -120) (.linkDistance 30) (.size clj->js [100 100]) (.nodes nodes) (.links links) )
-      svg (-> js/d3 (.select "div") (.append "svg") (.attr "width" width) (.attr "height" height))
-      link (-> svg (.selectAll ".link") (.data links) .enter (.append "line") (.attr "class" "link"))
-      node (-> svg (.selectAll ".node") (.data nodes) .enter (.append "circle") (.attr "class" "node"))
-      ]
-  (-> force (. on "end" 
-               (fn (-> node 
-                       (.attr "r" (/ width 25)) 
-                       (.attr "cx" (fn [d]  (.- d x))) 
-                       (.attr "cy" (fn [d] (.- d y))))))))
+nodes (clj->js 
+               [{:x (/ width 3) :y (/  height 2)} 
+                {:x (* 2 (/ width 3)) :y (/ height 2)}
+                {:x width :y height}])
 
 
-
-(let [rcolor (js/d3 -> .scale)])
-(def force  (-> js/d3 .-layout .force (.charge -120) (.linkDistance 30) (.size clj->js [100 100])))
 
--- src/cljs/chestnut/core.cljs
+++ src/cljs/chestnut/core.cljs
@@ -15,6 +15,79 @@
 (defn workingd3 []
   (-> js/d3 (.select "div") (.selectAll "p") (.data (clj->js ["hello" "world"])) .enter (.append "p") (.text "hello")))
 
+(defn makeashape [] 
+  (-> js/d3 
+      (.select "div") 
+      (.append "svg") 
+      (.attr "width" 500) 
+      (.attr "height" 500) 
+      (.selectAll "circle")
+      (.data (clj->js (range 10)))
+      .enter
+      (.append "circle")
+      (.attr "cy" (fn [d-] (* 10 d- d-))) 
+      (.attr "cx" 100) 
+      (.attr "r" (fn [d-] (* 10 d-)))))
+
+
+(defn getrandomnodes [n width height]
+  (clj->js (take n (repeatedly #(clj->js {:x (rand width) :y (rand height)})))))
+
+(defn getrandomlinks [n]
+  (clj->js (for [source (range n) 
+                 target (range n)
+                 :when (= 1 (rand-int 5))]
+             (clj->js {:source source :target target}))))
+
+
+(defn forcelayout []
+  (let [width 1000 height 1000
+        nodes (getrandomnodes 10 1000 1000)
+        links (getrandomlinks 10)
+        svg (-> js/d3 
+                (.select "div")
+                (.append "svg")
+                (.attr "width" width)
+                (.attr "height" height))
+        link (-> 
+              svg 
+              (.selectAll "line") 
+              (.data links)
+              .enter
+              (.append "line"))
+        node (->
+              svg
+              (.selectAll "circle")
+              (.data nodes)
+              .enter
+              (.append "circle")
+              (.attr "r" 100)
+              (.attr "cx" 100))
+        force (-> 
+               js/d3 
+               .-layout 
+               .force 
+               (.size #js [width height])
+               (.nodes nodes)
+               (.links links)
+               (.linkDistance (/ width 2))
+               (.on "end"  (fn [] (do 
+                                    (print "ended!")
+                                    (-> link
+                                        (.attr "x1" #(-> % .-source .-x))
+                                        (.attr "y1" #(-> % .-source .-y))
+                                        (.attr "x2" #(-> % .-target .-x))
+                                        (.attr "y2" #(-> % .-target .-y))
+                                        (.attr "stroke-width" 5)
+                                        (.attr "stroke" "red"))
+                                    (-> node
+                                        (.attr "r" 10)
+                                        (.attr "cx" #(.-x %) )
+                                        (.attr "cy" #(.-y %))))))
+               .start)]
+    ))
+
+
 (defn main []
   ()
   (om/root
@@ -28,7 +101,7 @@
             element))
         om/IDidMount
         ( did-mount [_]
-          (workingd3))))
+          (forcelayout))))
     app-state
     {:target (. js/document (getElementById "app"))}))
 
